c7170e67f7bff4f718e572d2b2513e418790280c
saavedra75 <josemanuel75saavedra@gmail.com>
2025-03-06 09:54:02 +0100

    creo la estructura de paquetes

---
+++ test/test/PersonaTest.java
@@ -0,0 +1,76 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import code.Persona;
+
+class PersonaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		Persona jose = new Persona("Jose", 20, 'H', 60.00, 162);
+		Persona carlos = new Persona();
+		Persona lola = new Persona();
+		Persona elias = new Persona();
+	
+	}
+
+	@Test
+	void testPersona() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPersonaStringIntChar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPersonaStringIntCharDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSexo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPeso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAltura() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalcularIMC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEsMayorDeEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
